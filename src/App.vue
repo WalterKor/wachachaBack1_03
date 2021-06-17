6b3773911f294bca42a8da126a0043a2789b7c7d
everglow <78077569+okchanho@users.noreply.github.com>
2021-06-17 23:15:05 +0900

    6월 17일 컴포넌트

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,98 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <div class="menu">
+      <a v-for="a in menu" :key="a">{{a}}</a>
+    </div>
+
+    <Banner/>
+
+    <div v-for="k in data" :key="k" class="okchanho">
+      <h3 @click="OpenModal = true; point = k">{{k.title}}</h3>
+      <img :src="k.image" class="foster-size">
+      <P @click="openMo == true">{{k.content}}</P>
+    </div>
+
+
+
+    <!--모달창만들기-->
+    <div class="black-bg" v-if="OpenModal = true">
+      <div class="white-bg">
+          <h4>{{ data[point].title }}</h4>
+          <img :src="data[point].image" class="foster">
+          <p> {{ data[point].content }}</p>
+          <button @click="OpenModal = false">닫기</button>
+      </div>
+      <button @click="OpenModal = false">닫기</button>
+    </div>
+
+    <div v-if= "openMo = true" class="black-bg">
+      <div class="white-bg">
+        <h4>안녕하세요</h4>
+        <button @click="openMo = false">닫기</button>
+      </div>
+    </div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import data from './assets/Movie.js';
+import Banner from './components/Banner.vue';
+
 
 export default {
   name: 'App',
+  data(){
+    return{
+      point : 0,
+      menu : ['MainHome', 'List', 'Login'],
+      data : data,
+      OpenModal : false,
+      openMo : false,
+    }
+  },
   components: {
-    HelloWorld
+    Banner : Banner,
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+ div{
+  box-sizing: border-box;
 }
+.menu{
+  background: coral;
+  padding: 20px;
+  border-radius: 10px;
+}
+
+.menu a{
+  color: white;
+  padding: 10px;
+  font-weight: bold;
+  text-align: center;
+}
+.foster-size{
+  height: 700px;
+  width: 450px;
+  display: inline-block;
+}
+
+.okchanho p{
+  cursor: pointer;
+}
+
+.black-bg{
+  width: 100%; height: 100%;
+  background: rgba(0, 0, 0, 0.5);
+  position: fixed; padding: 20px;
+}
+.white-bg{
+  width: 100%; background: white;
+  border-radius: 8px;
+  padding: 20px;
+}
+
+
 </style>
---
+++ src/components/Banner.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="banner">
+    <h3>까꿍</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    name : 'Banner',
+    data() {
+      return {
+
+      }
+    },
+    components : {
+
+    }
+}
+</script>
+
+<style>
+  .banner{
+
+    margin: 10px;
+    padding: 10px;
+    background: aliceblue;
+    border-radius: 5px;
+  }
+</style>
